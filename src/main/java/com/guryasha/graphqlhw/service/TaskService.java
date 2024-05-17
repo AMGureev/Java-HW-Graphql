0cb4bd6897951c391f6863ec0f32cf6ea58b3196
alexgureev <amgureev@edu.hse.ru>
2024-05-17 22:27:32 +0300

    final!

--- src/main/java/com/guryasha/graphqlhw/service/TaskService.java
+++ src/main/java/com/guryasha/graphqlhw/service/TaskService.java
@@ -4,5 +4,5 @@ import com.guryasha.graphqlhw.entity.TaskEntity;
 
 public interface TaskService {
     TaskEntity saveTask(String title, String desc); // save task on list and return it
-    Iterable<TaskEntity> returnTasks(); // return several tasks
+    Iterable<TaskEntity> returnTasks(); // return all tasks
 }
