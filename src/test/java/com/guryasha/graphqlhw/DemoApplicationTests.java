9fbe10a74d484aea5ffeef11786bdd254cba5565
alexgureev <amgureev@edu.hse.ru>
2024-05-17 21:30:32 +0300

    Initial commit

---
+++ src/test/java/com/guryasha/graphqlhw/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guryasha.graphqlhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
